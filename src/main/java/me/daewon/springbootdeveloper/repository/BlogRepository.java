ae916a513d2f0542d5f789063166a9a12bbebfb4
KimDevloper <a62387272@gmail.com>
2023-07-10 17:45:23 +0900

    [ADD] Spring으로 간단한 블로그 만들기

---
+++ src/main/java/me/daewon/springbootdeveloper/repository/BlogRepository.java
@@ -0,0 +1,7 @@
+package me.daewon.springbootdeveloper.repository;
+
+import me.daewon.springbootdeveloper.domain.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlogRepository extends JpaRepository<Article, Long> {
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('Title1', 'content1', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('Title2', 'content2', NOW(), NOW())
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('Title3', 'content3', NOW(), NOW())
